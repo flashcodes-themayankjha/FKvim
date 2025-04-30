1caaca6bef8b4833e34f102c3655ce90b2aeb7c7
flashcodes-themayankjha <the.mayank.k.jha@gmail.com>
2025-04-30 14:56:53 +0530

    Added  LSP Test Files

---
+++ lua/FKvim_rc/lsp-test/pythonlsp.py
@@ -0,0 +1,9 @@
+'''  before running this make sure you have installed python  lsp in  your system 
+
+npm install -g pyright
+which pyright
+
+check the documentation on our website 
+
+'''
+print("\n Testing Py ") 
