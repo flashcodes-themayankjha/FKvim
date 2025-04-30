1fae53321ca11c4175ae4d9e303916d0d42a799d
flashcodes-themayankjha <the.mayank.k.jha@gmail.com>
2025-04-30 14:53:20 +0530

    updated entry point

--- lua/FKvim_rc/core/init.lua
+++ lua/FKvim_rc/core/init.lua
@@ -1,3 +1,12 @@
+-- Enable mouse support
+vim.opt.mouse = "a"
+
+-- Use system clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- Better yank/paste with mouse or OS shortcuts
+vim.opt.paste = false
+
 require("FKvim_rc.core.options")
 require("FKvim_rc.core.keymaps")
 require("FKvim_rc.core.autocmd")
