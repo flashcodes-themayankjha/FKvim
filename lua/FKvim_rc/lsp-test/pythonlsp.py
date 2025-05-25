ce6c279a79a5140c89e82914bc5ea5a5daa5cf98
flashcodes-themayankjha <the.mayank.k.jha@gmail.com>
2025-05-25 06:47:25 +0530

    Add Winbar Breadcrumbs

--- lua/FKvim_rc/lsp-test/pythonlsp.py
+++ lua/FKvim_rc/lsp-test/pythonlsp.py
@@ -6,6 +6,57 @@ which pyright
 check the documentation on our website 
 
 '''
-print("\n Testing Py ") 
 
+print("hi")
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        return "..."
+
+    def info(self):
+        def inner_info():
+            return f"Name is {self.name}"
+        return inner_info()
+
+
+class Dog(Animal):
+    def __init__(self, name, breed):
+        super().__init__(name)
+        self.breed = breed
+
+    def speak(self):
+        return "Woof!"
+
+    def fetch(self):
+        def with_ball():
+            return "Fetching the ball"
+        def with_stick():
+            return "Fetching the stick"
+        return with_ball()
+
+
+class Cat(Animal):
+    def speak(self):
+        return "Meow"
+
+    def scratch(self):
+        class ScratchLevel:
+            def __init__(self, level):
+                self.level = level
+
+            def describe(self):
+                return f"Scratch level is {self.level}"
+        
+        lvl = ScratchLevel(3)
+        return lvl.describe()
+
+
+def standalone_function():
+    print("I am not part of any class")
+
+
+standalone_function()
 
